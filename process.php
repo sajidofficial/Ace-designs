9724e10d29396e3f3936fc9c41786cf5b426cb5b
sajidofficial <kazisajid14@gmail.com>
2020-11-05 12:12:52 +0530

    1

--- process.php
+++ process.php
@@ -1,5 +1,25 @@
 <?php
 
-    use PHPMailer\PHPMailer\PHPMailer;
-    
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email-from = 'kazisajid14@gmail.com';
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "User Name: $name.\n".
+                  "User Email: $visitor_email.\n".
+                  "User Message: $message.\n";
+
+        $to = "kazisajid14@gmail.com";
+
+        $headers = "From: $email_from \r\n";
+
+        $headers .= "Reply-To: $visitor_email \r\n";
+
+        mail($to,$email_subject,$email_body,$headers);
+
+        header("Location: index.html");
+        
 ?>
